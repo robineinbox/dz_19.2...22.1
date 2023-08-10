beb6f2e9cfc01483e2c2ff69345ab078554c1784
robineinbox <robin_e@inbox.ru>
2023-08-10 13:14:55 +0300

    initial commit

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:product_id>/', views.product, name='product'),
+    path('great_prod/', views.great_prod, name='great_prod'),
 ]
-
--- main/views.py
+++ main/views.py
@@ -1,9 +1,45 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from datetime import datetime
+from main.models import Product
 
 def home(request):
     return render(request, 'home.html')
 
-
 def contact(request):
     return render(request, 'contact.html')
+
+def index(request):
+    cat_list = Product.objects.all()
+    context = {
+        'object_list': cat_list
+    }
+    return render(request, 'main/index.html', context)
+
+def contacts(request):
+    """Контроллер, который отвечает за отображение контактной информации."""
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(name, phone, message)
+    return render(request, 'contact.html')
+
+def product(request, product_id):
+    """представление страницы main/product.html для каждого продукта"""
+    prod_get = get_object_or_404(Product, pk=product_id)
+    return render(request, 'main/product.html', {'product': prod_get})
+
+def great_prod(request):
+    """Представление страницы main/great_prod.html с формой загрузки нового продукта"""
+    if request.method == 'POST':
+        product = {
+            'name_prod': request.POST.get('name'),
+            'description_prod': request.POST.get('description'),
+            'category_prod': request.POST.get('category'),
+            'price_prod': request.POST.get('price'),
+            'data_create_prod': datetime.now(),
+            'data_change_prod': datetime.now()
+        }
+        # загрузка нового продукта в БД
+        Product.objects.create(**product)
+    return render(request, 'main/great_prod.html')
