deb60c9defdb71c9d0c633377e1a1e9753c2696e
robineinbox <robin_e@inbox.ru>
2023-08-15 00:54:12 +0300

    добавлены исправления

--- main/templatetags/media.py
+++ main/templatetags/media.py
@@ -1,14 +1,3 @@
-# from django import template
-# from django.conf import settings
-#
-# register = template.Library()
-#
-# @register.filter
-# def media_url(value):
-#     return f"{settings.MEDIA_URL}{value}"
-
-
-
 from django import template
 from django.conf import settings
 
@@ -16,6 +5,4 @@ register = template.Library()
 
 @register.filter
 def media_url(value):
-    if value.startswith('/'):
-        value = value[1:]
     return f"{settings.MEDIA_URL}{value}"
